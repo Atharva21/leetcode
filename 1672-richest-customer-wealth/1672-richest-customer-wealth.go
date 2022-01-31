1db29e84a9a3a08811a9440070774f6b20047803
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-01-31 15:31:11 +0530

    Time: 10 ms (16.42%), Space: 3.7 MB (15.92%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.go
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.go
@@ -1,19 +1,32 @@
-// var v []int
+var v [51]int
+var acc *[][]int
+var wg sync.WaitGroup
+var n int
+func test(idx int) {
+    // fmt.Println((*acc)[idx][0])
+    ans := 0
+    for i:=0; i<n;i++ {
+        ans += (*acc)[idx][i]
+    }
+    v[idx] = ans
+}
 
 func maximumWealth(accounts [][]int) int {
+    acc = &accounts
     m := len(accounts)
-    n := len(accounts[0])
-    // fmt.Println(m, n)
-    // v := make([]int, m)
-    // fmt.Println(v)
+    n = len(accounts[0])
     ans := 0
     for i := 0; i<m; i++ {
-        sum := 0
-        for j := 0; j < n; j++ {
-            sum += accounts[i][j]
-        }
-        if sum > ans {
-            ans = sum;
+        wg.Add(1)
+        go func(i int) {
+            test(i)
+            wg.Done()
+        }(i)
+    }
+    wg.Wait()
+    for i:=0; i<m;i++ {
+        if v[i] > ans {
+            ans = v[i]
         }
     }
     return ans
