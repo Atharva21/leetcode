1e6c3339e3a163bcb8dd66641d2b2fdbbb1f1a1d
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-02-01 11:04:21 +0530

    Time: 111 ms (22.98%), Space: 55.4 MB (92.28%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,36 +1,11 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        const int n = nums.size();
-        int ans = 0, left = 0, right = 0;
-        int flips = 0;
-        while(left < n && right < n) {
-            // cout << "~~~~" << left << " ~~ " << right << endl;
-            if(nums[right]) {
-                ans = max(ans, right-left+1);
-                // cout << ans << "# " << left << " " << right << endl;
-                ++right;
-            } else {
-                if(flips < k) {
-                    // flip and go ahead.
-                    ++flips;
-                    ans = max(ans, right-left+1);
-                    // cout << ans << "## " << left << " " << right << endl;
-                    ++right;
-                } else {
-                    // shrink.
-                    if(!nums[left] && flips) {
-                        --flips;
-                    }
-                    if(left == right) {
-                        ++left;
-                        ++right;
-                    } else {
-                        ++left;
-                    }
-                }
-            }
+    int longestOnes(vector<int>& A, int K) {
+        int i = 0, j;
+        for (j = 0; j < A.size(); ++j) {
+            if (A[j] == 0) K--;
+            if (K < 0 && A[i++] == 0) K++;
         }
-        return ans;
+        return j - i;
     }
 };
