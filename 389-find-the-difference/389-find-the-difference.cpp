9c5d6d6de85c319998c55c3ef0cf9107788a20c9
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-02-07 10:48:35 +0530

    Time: 3 ms (78.44%), Space: 6.9 MB (9.15%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -5,7 +5,6 @@ public:
         for(auto& c: s) ++mp1[c];
         for(auto& c: t) ++mp2[c];
         for(auto& p: mp2) {
-            if(!mp1[p.first]) return p.first;
             if(mp1[p.first] < p.second) return p.first;
         }
         return ' ';
