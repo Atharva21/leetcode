e2218c111f923ff126ae2c1dadc2fb966ea286e8
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-02-07 10:47:47 +0530

    Time: 2 ms (79.33%), Space: 6.8 MB (9.15%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> mp1, mp2;
+        for(auto& c: s) ++mp1[c];
+        for(auto& c: t) ++mp2[c];
+        for(auto& p: mp2) {
+            if(!mp1[p.first]) return p.first;
+            if(mp1[p.first] < p.second) return p.first;
+        }
+        return ' ';
+    }
+};
