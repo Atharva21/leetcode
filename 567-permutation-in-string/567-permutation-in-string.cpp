0647d4b204aa297364f16a8c87764133a5761e1d
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-02-11 07:05:28 +0530

    Time: 275 ms (10.65%), Space: 15.9 MB (11.78%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -10,9 +10,7 @@ public:
             if(m1[s2[i]-'a'] == 0) continue;
             vector<int> m2(26, 0);
             for(int j=0;j<m;++j) {
-                int x = s2[i+j]-'a';
-                ++m2[x];
-                if(m1[x] < m2[x]) break;
+                ++m2[s2[i+j]-'a'];
             }
             if(m1 == m2) return true;
         }
