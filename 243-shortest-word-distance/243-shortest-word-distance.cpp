4f0ba711093aeefd887b24aa145eddc865786432
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-04-21 11:23:35 +0530

    Time: 8 ms (90.68%), Space: 11.7 MB (50.53%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.cpp
+++ 243-shortest-word-distance/243-shortest-word-distance.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
-        unordered_map<string, vector<int>> mp;
-        int x = 0;
-        for(auto& word: wordsDict) mp[word].push_back(x++);
-        auto v1 = mp[word1];
-        auto v2 = mp[word2];
+    int shortestDistance(vector<string>& words, string a, string b) {
         int ans = INT_MAX;
-        int i=0,j=0;
-        int m = v1.size(),n=v2.size();
-        while(i<m&&j<n) {
-            ans = min(ans, abs(v1[i]-v2[j]));
-            if(v1[i] < v2[j]) ++i;
-            else ++j;
+        int lasta = -1, lastb = -1;
+        const int n = words.size();
+        for(int i=0;i<n;++i) {
+            const string word = words[i];
+            if(word == a) {
+                lasta = i;
+                if(lastb == -1) {
+                    continue;
+                }
+                ans = min(ans, lasta-lastb);
+            } else if(word == b) {
+                lastb = i;
+                if(lasta == -1) {
+                    continue;
+                }
+                ans = min(ans, lastb-lasta);
+            }
         }
         return ans;
     }
