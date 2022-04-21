02843f29a53a8758e862e5f07623d549c3ff5811
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-04-21 11:37:04 +0530

    Time: 15 ms (49.02%), Space: 11.6 MB (50.53%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.cpp
+++ 243-shortest-word-distance/243-shortest-word-distance.cpp
@@ -6,19 +6,20 @@ public:
         const int n = words.size();
         for(int i=0;i<n;++i) {
             const string word = words[i];
+            if(word != a && word != b) continue;
+            int *updateIndex, *compareIndex;
             if(word == a) {
-                lasta = i;
-                if(lastb == -1) {
-                    continue;
-                }
-                ans = min(ans, lasta-lastb);
-            } else if(word == b) {
-                lastb = i;
-                if(lasta == -1) {
-                    continue;
-                }
-                ans = min(ans, lastb-lasta);
+                updateIndex = &lasta;
+                compareIndex = &lastb;
+            } else {
+                updateIndex = &lastb;
+                compareIndex = &lasta;
             }
+            *updateIndex = i;
+            if(*compareIndex == -1) {
+                continue;
+            }
+            ans = min(ans, *updateIndex-*compareIndex);
         }
         return ans;
     }
