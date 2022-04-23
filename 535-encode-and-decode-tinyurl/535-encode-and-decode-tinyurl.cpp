b333bff833718dd7915f3227ac50c384c0219e5c
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-04-23 11:05:27 +0530

    Time: 7 ms (39.83%), Space: 7.9 MB (5.80%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,26 @@
 class Solution {
+private:
+    unordered_map<string, string> short2long, long2short;
+    const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890";
 public:
-
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        string shortUrl = "https://www.tinyurl.com/";
+        string code;
+        do {
+            code = "";
+            for(int i=0;i<6;++i) {
+                code += letters[rand()%letters.size()];
+            }
+        }while(short2long.find(shortUrl + code) != short2long.end());
+        short2long[shortUrl + code] = longUrl;
+        return long2short[longUrl] = shortUrl + code;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        cout << "got: " << shortUrl;
+        return short2long[shortUrl];
     }
 };
 
