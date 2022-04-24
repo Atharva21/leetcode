ee1325b3866a3cafaacb80f18d94b4decbd382ac
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-04-24 11:13:12 +0530

    Time: 208 ms (53.84%), Space: 59.3 MB (46.05%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,8 +1,8 @@
 class UndergroundSystem {
 private:
-    map<string, unordered_map<string, pair<double, int>>> mp;
+    unordered_map<string, pair<double, int>> mp;
     // from station -> to station -> moving average & sum.
-    map<int, pair<string, int>> checkins;
+    unordered_map<int, pair<string, int>> checkins;
     // id -> from station & start time.
 public:
     UndergroundSystem() {
@@ -18,24 +18,23 @@ public:
         string fromStation = resp.first;
         int startTime = resp.second;
         int currentTime = t - startTime;
-        
-        auto endStations = mp[fromStation];
-        if(endStations.find(stationName) != endStations.end()) {
+        string route = fromStation + "->" + stationName;
+        if(mp.find(route) != mp.end()) {
             // we already have a moving avg
-            auto avg = endStations[stationName];
+            auto avg = mp[route];
             double movingAvg = avg.first;
             int count = avg.second;
             double newAverage = ((movingAvg * count) + currentTime) / ((double)(count + 1));
-            endStations[stationName] = {newAverage, count+1};
-            mp[fromStation] = endStations;
+            mp[route] = {newAverage, count+1};
         } else {
             // first time we got this end station.
-            mp[fromStation][stationName] = {currentTime, 1};
+            mp[route] = {currentTime, 1};
         }
     }
     
     double getAverageTime(string startStation, string endStation) {
-        return mp[startStation][endStation].first;
+        string route = startStation + "->" + endStation; 
+        return mp[route].first;
     }
 };
 
