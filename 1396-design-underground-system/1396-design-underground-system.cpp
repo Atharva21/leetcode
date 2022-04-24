150778ea090b7379dc9ec43a7c1b9ce2022c5a22
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-04-24 11:03:40 +0530

    Time: 395 ms (5.19%), Space: 105.1 MB (5.08%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -0,0 +1,48 @@
+class UndergroundSystem {
+private:
+    unordered_map<string, unordered_map<string, pair<double, int>>> mp;
+    // from station -> to station -> moving average & sum.
+    unordered_map<int, pair<string, int>> checkins;
+    // id -> from station & start time.
+public:
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        checkins[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        auto resp = checkins[id];
+        string fromStation = resp.first;
+        int startTime = resp.second;
+        int currentTime = t - startTime;
+        
+        auto endStations = mp[fromStation];
+        if(endStations.find(stationName) != endStations.end()) {
+            // we already have a moving avg
+            auto avg = endStations[stationName];
+            double movingAvg = avg.first;
+            int count = avg.second;
+            double newAverage = ((movingAvg * count) + currentTime) / ((double)(count + 1));
+            endStations[stationName] = {newAverage, count+1};
+            mp[fromStation] = endStations;
+        } else {
+            // first time we got this end station.
+            mp[fromStation][stationName] = {currentTime, 1};
+        }
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        return mp[startStation][endStation].first;
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
