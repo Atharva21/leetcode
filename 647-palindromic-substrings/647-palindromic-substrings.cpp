b7fb45b739ff7095beddc3891031f0af9de7bab9
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-05-23 09:54:49 +0530

    Time: 8 ms (67.77%), Space: 6.3 MB (66.37%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -18,7 +18,9 @@ public:
         n = s.size();
         int ans = n;
         for(int i=0;i<n;++i) {
+            // for odd palindromes
             ans += palindromeCount(i-1, i+1);
+            // for even palindromes
             ans += palindromeCount(i, i+1);
         }
         return ans;
