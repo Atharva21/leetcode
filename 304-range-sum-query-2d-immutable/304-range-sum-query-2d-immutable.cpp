7fa8bbec0d286a7db42004f3a56bbb7aaafbc98f
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-06-03 07:33:53 +0530

    Time: 458 ms (82.67%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -6,19 +6,29 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         m = matrix.size();
         n = m == 0 ? 0 : matrix[0].size();
-        dp.resize(m, vector<int>(n+1, 0));
+        dp.resize(m+1, vector<int>(n+1, 0));
         for(int i=0;i<m;++i) {
-            for(int j=1;j<=n;++j) {
-                dp[i][j] = dp[i][j-1] + matrix[i][j-1];
+            for(int j=0;j<n;++j) {
+                dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + matrix[i][j];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans = 0;
-        for(int i=row1;i<=row2;++i) {
-            ans += dp[i][col2+1] - dp[i][col1];
+        int ans = dp[row2+1][col2+1];
+        if(col1 > 0) {
+            // not connected to left edge.
+            ans -= (dp[row2+1][col1]);
         }
+        if(row1 > 0) {
+            // not connected to the top.
+            ans -= (dp[row1][col2+1]);
+        }
+        if(col1 > 0 && row1 > 0) {
+            ans += dp[row1][col1];
+        }
+        // cout << ans << endl;
+        // cout << dp[row1][col1];
         return ans;
     }
 };
