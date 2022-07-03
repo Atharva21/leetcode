5d6e0e8c5e71b954f7add1e29cc1d754dc684f5b
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-07-03 10:32:26 +0530

    Time: 3 ms (69.26%), Space: 7.3 MB (19.43%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
-private:
-    vector<int> v;
-    int n;
-    int dp[1002][1003][2];
-    int dfs(bool greater, int i=0, int last=-1) {
-        if(i >= n) return 0;
-        if(dp[i][last+1][greater] != -1) return dp[i][last+1][greater];
-        int notChoose = dfs(greater, i+1, last);
-        int choose = INT_MIN;
-        if(last == -1 || ((greater && v[i] > v[last]) || !greater && v[i] < v[last])) {
-            choose = 1 + dfs(!greater, i+1, i);
-        }
-        return dp[i][last+1][greater] = max(choose, notChoose);
-        // return max(choose, notChoose);
-    }
 public:
-    int wiggleMaxLength(vector<int>& nums) {
-        v = nums;
-        n = v.size();
-        memset(dp, -1, sizeof(dp));
-        return max(dfs(true), dfs(false));
+    int wiggleMaxLength(vector<int> nums) {
+        if (nums.size() < 2)
+            return nums.size();
+        int prevdiff = nums[1] - nums[0];
+        int count = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < nums.size(); i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                count++;
+                prevdiff = diff;
+            }
+        }
+        return count;
     }
 };
