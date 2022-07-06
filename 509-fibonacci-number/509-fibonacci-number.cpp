9316296a1fd866731d3e844ce96a5c36120a646f
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-07-06 08:22:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
+private:
+    int dp[32];
 public:
+    Solution() {
+        memset(dp, -1, sizeof(dp));
+    }
     int fib(int n) {
-        if(n < 2) return n;
-        int f0 = 0, f1 = 1, f2 = 1;
-        for(int i=2;i<=n;++i) {
-            f2 = f0+f1;
-            f0 = f1;
-            f1 = f2;
-        }
-        return f1;
+        if(n < 2) return dp[n] = n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fib(n-1) + fib(n-2);
     }
 };
