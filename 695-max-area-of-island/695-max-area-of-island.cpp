17515368860ead31b7dd57b6f346c15cd43a4cff
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-07-15 09:03:59 +0530

    Time: 30 ms (54.31%), Space: 25 MB (19.69%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,50 +1,38 @@
 class Solution {
 private:
-    int m, n;
     vector<vector<int>> grid;
-    bool vis[51][51];
+    vector<vector<bool>> vis;
+    int m, n;
     vector<pair<int, int>> dirs = {
-        {-1, 0},
         {1, 0},
         {0, 1},
+        {-1, 0},
         {0, -1}
     };
     bool valid(int i, int j) {
-        return i >= 0 && i < m && j >= 0 && j < n;
+        return i>=0&&i<m&&j>=0&&j<n;
     }
-    int bfs(int i, int j) {
-        queue<pair<int, int>> q;
-        q.push({i, j});
+    int dfs(int i, int j) {
+        if(!valid(i, j) || !grid[i][j] || vis[i][j]) return 0;
         vis[i][j] = true;
-        int area = 0;
-        while(!q.empty()) {
-            auto pr = q.front();
-            // cout << pr.first << " " << pr.second << " # ";
-            q.pop();
-            ++area;
-            for(auto& dir: dirs) {
-                int newx = pr.first + dir.first;
-                int newy = pr.second + dir.second;
-                if(valid(newx, newy) && grid[newx][newy] == 1 && !vis[newx][newy]) {
-                    q.push({newx, newy});
-                    vis[newx][newy] = true;
-                }
-            }
+        int sum = 0;
+        for(auto& dir: dirs) {
+            sum += dfs(i + dir.first, j + dir.second);
         }
-        // cout << " = " << area << endl;
-        return area;
+        return 1 + sum;
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        this -> grid = grid;
+        this->grid = grid;
         m = grid.size();
         n = grid[0].size();
-        memset(vis, false, sizeof(vis));
+        vis.resize(m, vector<bool>(n, false));
         int ans = 0;
         for(int i=0;i<m;++i) {
             for(int j=0;j<n;++j) {
-                if(grid[i][j] == 0 || vis[i][j]) continue;
-                ans = max(ans, bfs(i, j));
+                if(grid[i][j] && !vis[i][j]) {
+                    ans = max(ans, dfs(i, j));
+                }
             }
         }
         return ans;
