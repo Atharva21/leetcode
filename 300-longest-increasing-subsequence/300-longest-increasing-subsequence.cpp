71448616db06bbffa6be7cfbe990150784d2648d
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-08-08 16:04:09 +0530

    Time: 12 ms (88.85%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,6 +3,7 @@ private:
     // int dp[2505][2505];
     // vector<int> v;
     // int n;
+    // \U0001f447 2^n complexity.
     // int dfs(int i=0, int last=-1) {
     //     if(i >= n) return 0;
     //     if(dp[i][last+1] != -1) return dp[i][last+1];
@@ -19,6 +20,7 @@ public:
         // n = v.size();
         // memset(dp, -1, sizeof(dp));
         // return dfs();
+        // \U0001f447 nlogn complexity
         vector<int> v;
         for(auto& num: nums) {
             auto it = lower_bound(v.begin(), v.end(), num);
