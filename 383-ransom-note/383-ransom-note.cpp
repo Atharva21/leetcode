ad804a6ddc1e4702bdbdd03cd276ce3e0160aab4
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-08-25 06:57:44 +0530

    Time: 21 ms (65.89%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<char, int> mp;
         for(auto& c: magazine) ++mp[c];
         for(auto& c: ransomNote) {
-            auto& i = mp[c];
+            auto& i = mp[c]; // a bit faster than writing mp[c], and --mp[c] for 2 lookups
             if(!i) return false;
             --i;
         }
