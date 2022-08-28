f45a1a573e41447d211fa012b6f4fd19e926981e
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-08-28 19:18:48 +0530

    Time: 962 ms (30.25%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -9,15 +9,18 @@
 #     Employees.name not like 'M%'
 # order by Employees.employee_id
 # ;
-select
-    Employees.employee_id,
-    case when
-    Employees.employee_id % 2 = 1
-    and
-    Employees.name not like 'M%'
-    then Employees.salary
-    else 0
-    end  as 'bonus'
-from
-    Employees
-order by Employees.employee_id
+# select
+#     Employees.employee_id,
+#     case when
+#     Employees.employee_id % 2 = 1
+#     and
+#     Employees.name not like 'M%'
+#     then Employees.salary
+#     else 0
+#     end  as 'bonus'
+# from
+#     Employees
+# order by Employees.employee_id
+select employee_id, 
+if(employee_id%2 != 0 and name NOT LIKE 'M%', salary, 0) as bonus
+from employees order by employee_id;
