b1488493a6e747dec7c197aaefbe032405ff8522
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-08-29 11:17:57 +0530

    Time: 388 ms (54.11%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 select p.patient_id, p.patient_name, p.conditions
 from Patients as p
-where p.conditions regexp '\\bDIAB1';
+where p.conditions like '% DIAB1%' or
+p.conditions like 'DIAB1%';
