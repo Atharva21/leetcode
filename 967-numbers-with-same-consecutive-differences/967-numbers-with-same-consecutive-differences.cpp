6e0800cc3a97723d4360fb8be19449ede1ad8189
Atharva Mulay <35420813+Atharva21@users.noreply.github.com>
2022-09-03 09:39:17 +0530

    Time: 3 ms (74.35%), Space: 8.9 MB (15.94%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -2,17 +2,18 @@ class Solution {
 private:
     vector<int> ans;
     int diff;
-    unordered_map<int, bool> vis[10];
+    // unordered_map<int, bool> vis[10];
     void permute(int last, int cur, int pos) {
-        if(vis[pos+1][cur]) return;
-        vis[pos+1][cur] = true;
+        // if(vis[pos+1][cur]) return;
+        // vis[pos+1][cur] = true;
         if(last < 0 || last > 9) return;
         if(pos < 0) {
             ans.push_back(cur);
             return;
         }
         permute(last+diff, cur+((last+diff)*pow(10, pos)), pos-1);
-        permute(last-diff, cur+((last-diff)*pow(10, pos)), pos-1);
+        if(diff > 0)
+            permute(last-diff, cur+((last-diff)*pow(10, pos)), pos-1);
     }
 public:
     vector<int> numsSameConsecDiff(int n, int k) {
